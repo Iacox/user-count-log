dad23dd5a7e7ad8b81633c885a848bfaea9b6c23
Portulak <ilyshk999@gmail.com>
2021-04-17 22:26:03 +0300

    - added input - added methods to save timestamps - added methods to save request logs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="input-field">
+    <input
+      @keyup="typingLog"
+      @blur="countStart"
+      @keyup.enter="$refs.input.blur"
+      v-model.lazy="text"
+      ref="input"
+      id="text"
+      type="text"
+    >
+    <label for="text">Type here</label>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'StartPage',
+  data() {
+    return {
+      text: '',
+      timeStamp: 0,
+      arrMarkTime: [],
+      requestLog: [],
+      requestInterval: null,
+    }
+  },
+  mounted() {
+    this.countStart()
+  },
+  methods: {
+    countStart() {
+      if (this.arrMarkTime.length)
+        this.sendRequest()
+
+      if (this.requestInterval)
+        clearInterval(this.requestInterval)
+
+      this.$refs.input.addEventListener('keydown', () => {
+        this.timeStamp = +new Date
+        this.requestInterval = setInterval(this.sendRequest, 60000)
+      }, {once: true})
+    },
+    typingLog() {
+      const countedTime = `${new Date(new Date - this.timeStamp).getSeconds()}:${new Date(new Date - this.timeStamp).getMilliseconds()}`
+      this.arrMarkTime.push(countedTime)
+    },
+    async sendRequest() {
+      const userTypeLog = JSON.stringify(this.arrMarkTime)
+
+      this.arrMarkTime = []
+      this.timeStamp = +new Date
+
+      const request = await fetch('/api', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: userTypeLog,
+      })
+      this.requestLog.push(request)
+    },
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+  .input-field {
+    margin-top: 12rem;
+    width: 100%;
+  }
+</style>
